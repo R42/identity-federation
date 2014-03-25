91483453a8314a2f82871e3ef4ce7081cf7e93d3
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-03-25 13:21:16 +0000

    watson service change cors order

--- node-service/server.js
+++ node-service/server.js
@@ -4,15 +4,18 @@ var valid = require('./validator');
 
 var app = express()
   .use(express.logger('dev'))
-  .use(authenticator)
-  .use(cors());
+  .use(cors())
+  .use(authenticator);
+  
 
-app.get('/', function(req, res) { res.send('Hello!'); });
+app.get('/', function(req, res) { 
+  res.send('Mr Watson Speaking'); 
+});
 
 var tokenRegex = /^\s*token\s+/i;
 function authenticator(req, res, next) {
   var auth = req.header('Authorization');
-  
+
   if (! auth)
     return res.send(401, 'Missing Authorization header');
 
@@ -20,11 +23,14 @@ function authenticator(req, res, next) {
     return res.send(401, 'Invalid Authorization scheme');
 
   var token = auth.replace(tokenRegex, '');
+
   if (! valid(token))
     return res.send(401, 'Invalid Authorization');
 
   next();
 }
 
-app.listen(9000);
+app.listen(9000, function(){
+  console.log('Watson quotes Service on port 9000.')
+});
 
