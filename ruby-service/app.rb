a2c76a40f11d2d6c7634b29c1f862eb34fa7dfa2
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-03-25 17:06:58 +0000

    holmes quotes

--- ruby-service/app.rb
+++ ruby-service/app.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
 require 'authenticate'
 
+
+QUOTES = [
+  'My name is Sherlock Holmes.  It is my business to know what other people dont know',
+  'I am the last and highest court of appeal in detection',
+  'There is nothing like first-hand evidence.'
+]
+
 class SherlockGreetings < Sinatra::Base
   
   get '/' do 
-    "hello from baker street"
+    QUOTES.sample
   end
 
 end
